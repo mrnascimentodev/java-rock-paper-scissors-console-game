2ec01338e9aa85b6eb02c9ff37691a03bd9a73bb
Marcos Roberto <mrnascimento.dev@gmail.com>
2025-08-27 13:56:36 +0000

    feat: scoreboard model

---
+++ src/main/java/br/com/madda/rock_paper_scissors/model/Scoreboard.java
@@ -0,0 +1,41 @@
+package br.com.madda.rock_paper_scissors.model;
+
+public class Scoreboard {
+  private int playerVictories;
+  private int computerVictories;
+  private int draws;
+
+  public void addVictory() {
+    playerVictories++;
+  }
+
+  public void addDefeat() {
+    computerVictories++;
+  }
+
+  public void addTie() {
+    draws++;
+  }
+
+  public int getPlayerVictories() {
+    return playerVictories;
+  }
+
+  public int getComputerVictories() {
+    return computerVictories;
+  }
+
+  public int getDraws() {
+    return draws;
+  }
+
+  public int getTotalGames() {
+    return getPlayerVictories() + getComputerVictories() + getDraws();
+  }
+
+  public void reset() {
+    playerVictories = 0;
+    computerVictories = 0;
+    draws = 0;
+  }
+}
