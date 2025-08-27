5a2a2c141dd26f5f65432a640e1b9caa3602f57a
Marcos Roberto <mrnascimento.dev@gmail.com>
2025-08-27 14:15:09 +0000

    feat: scoreboard dto

---
+++ src/main/java/br/com/madda/rock_paper_scissors/dto/ScoreboardDTO.java
@@ -0,0 +1,59 @@
+package br.com.madda.rock_paper_scissors.dto;
+
+public class ScoreboardDTO {
+  private String playerName;
+  private int victories;
+  private int defeats;
+  private int draws;
+  private int totalGames;
+
+  public ScoreboardDTO() {}
+
+  public ScoreboardDTO(String playerName, int victories, int defeats, int draws, int totalGames) {
+    this.playerName = playerName;
+    this.victories = victories;
+    this.defeats = defeats;
+    this.draws = draws;
+    this.totalGames = totalGames;
+  }
+
+  public String getPlayerName() {
+    return playerName;
+  }
+
+  public void setPlayerName(String playerName) {
+    this.playerName = playerName;
+  }
+
+  public int getVictories() {
+    return victories;
+  }
+
+  public void setVictories(int victories) {
+    this.victories = victories;
+  }
+
+  public int getDefeats() {
+    return defeats;
+  }
+
+  public void setDefeats(int defeats) {
+    this.defeats = defeats;
+  }
+
+  public int getDraws() {
+    return draws;
+  }
+
+  public void setDraws(int draws) {
+    this.draws = draws;
+  }
+
+  public int getTotalGames() {
+    return totalGames;
+  }
+
+  public void setTotalGames(int totalGames) {
+    this.totalGames = totalGames;
+  }
+}
