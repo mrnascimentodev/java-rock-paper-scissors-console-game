f77a422a7ca209fd7e21067ec3570ced0ebedc48
Marcos Roberto <mrnascimento.dev@gmail.com>
2025-08-27 13:49:29 +0000

    feat: result enum

---
+++ src/main/java/br/com/madda/rock_paper_scissors/model/enums/Result.java
@@ -0,0 +1,15 @@
+package br.com.madda.rock_paper_scissors.model.enums;
+
+public enum Result {
+  VICTORY("You won!"), DEFEAT("You lost!"), TIE("Tied!");
+
+  private final String message;
+
+  Result(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
