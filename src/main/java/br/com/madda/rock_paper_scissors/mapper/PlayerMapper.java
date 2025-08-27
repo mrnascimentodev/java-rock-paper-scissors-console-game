5e1840c3b76ba02506db1662f38284c0b9e29fd5
Marcos Roberto <mrnascimento.dev@gmail.com>
2025-08-27 14:20:11 +0000

    feat: player mapper

---
+++ src/main/java/br/com/madda/rock_paper_scissors/mapper/PlayerMapper.java
@@ -0,0 +1,29 @@
+package br.com.madda.rock_paper_scissors.mapper;
+
+import br.com.madda.rock_paper_scissors.dto.PlayerDTO;
+import br.com.madda.rock_paper_scissors.model.Player;
+
+public class PlayerMapper {
+  public PlayerDTO toDto(Player player) {
+    if (player == null) {
+      return null;
+    }
+
+    return new PlayerDTO(player.getId(), player.getName(), player.getCreatedAt(),
+        player.getUpdatedAt());
+  }
+
+  public Player toEntity(PlayerDTO dto) {
+    if (dto == null) {
+      return null;
+    }
+
+    Player player = new Player();
+    player.setId(dto.getId());
+    player.setName(dto.getName());
+    player.setCreatedAt(dto.getCreatedAt());
+    player.setUpdatedAt(dto.getUpdatedAt());
+
+    return player;
+  }
+}
--- src/main/java/br/com/madda/rock_paper_scissors/model/Player.java
+++ src/main/java/br/com/madda/rock_paper_scissors/model/Player.java
@@ -21,6 +21,10 @@ public class Player {
     return id;
   }
 
+  public void setId(Long id) {
+    this.id = id;
+  }
+
   public String getName() {
     return name;
   }
@@ -31,6 +35,10 @@ public class Player {
 
   public LocalDateTime getCreatedAt() {
     return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
   }
 
   public LocalDateTime getUpdatedAt() {
